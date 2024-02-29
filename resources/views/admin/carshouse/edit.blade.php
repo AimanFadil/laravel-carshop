09b55691857f0b82b9e69787d46bfe113ec1b451
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-02-29 16:53:11 +0100

    feat: added errors in carshouse create and edit

--- resources/views/admin/carshouse/edit.blade.php
+++ resources/views/admin/carshouse/edit.blade.php
@@ -8,30 +8,84 @@
         </div>
     </div>
     <div class="row">
+        <div class="col-12 form-group">
+            @if ($errors->any())
+                <div class="alert alert-warning" role="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
         <div class="col-12">
             <form action="{{ route('admin.carshouse.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
 
-                <label for="name">Aggiungi il Nome</label>
-                <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-contorl" required>
+                <div>
+                    <label for="name">Aggiungi il Nome</label>
+                    <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-contorl @error('name') is-invalid @enderror" required>
+    
+                    @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <label for="address">Aggiungi un indirizzo</label>
-                <input type="text" name="address" id="address" value="{{ old('address') }}" class="form-contorl" required>
+                <div>
+                    <label for="address">Aggiungi un indirizzo</label>
+                    <input type="text" name="address" id="address" value="{{ old('address') }}" class="form-contorl @error('address') is-invalid @enderror" required>
 
-                <label for="phone">Inserisci numero di telfono</label>
-                <input type="number" name="phone" id="phone" value="{{ old('phone') }}" class="form-contorl" required>
+                    @error('address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <label for="type">Tipologia</label>
-                <input type="text" name="type" id="type" value="{{ old('type') }}" class="form-contorl" required>
+                <div>
+                    <label for="phone">Inserisci numero di telfono</label>
+                    <input type="number" name="phone" id="phone" value="{{ old('phone') }}" class="form-contorl @error('phone') is-invalid @enderror" required>
 
-                <label for="mail">Inserisci email</label>
-                <input type="email" name="mail" id="mail" value="{{ old('mail') }}" class="form-contorl" required>
+                    @error('phone')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                <label for="social">Social</label>
-                <input type="text" name="social" id="social" value="{{ old('social') }}" class="form-contorl" required>
+                <div>
+                    <label for="type">Tipologia</label>
+                    <input type="text" name="type" id="type" value="{{ old('type') }}" class="form-contorl @error('type') is-invalid @enderror" required>
 
-                <label for="logo">Aggiungi il logo</label>
-                <input type="file" name="logo" id="logo" class="form-contorl" required>
+                    @error('type')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="mail">Inserisci email</label>
+                    <input type="email" name="mail" id="mail" value="{{ old('mail') }}" class="form-contorl @error('mail') is-invalid @enderror" required>
+
+                    @error('mail')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="social">Social</label>
+                    <input type="text" name="social" id="social" value="{{ old('social') }}" class="form-contorl @error('social') is-invalid @enderror" required>
+
+                    @error('social')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div>
+                    <label for="logo">Aggiungi il logo</label>
+                    <input type="file" name="logo" id="logo" class="form-contorl @error('logo') is-invalid @enderror" required>
+
+                    @error('logo')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
                 <button type="submit" class="btn btn-sm btn-success">Salva</button>
             </form>
