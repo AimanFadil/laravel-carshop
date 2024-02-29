06c3db53742fc5a4435743c6e90d5fffa7d9a642
Fabio9494 <fabio.casoria94@gmail.com>
2024-02-29 16:53:18 +0100

    request

--- app/Http/Requests/StoreCarRequest.php
+++ app/Http/Requests/StoreCarRequest.php
@@ -26,14 +26,15 @@ class StoreCarRequest extends FormRequest
 
         return [
             'modello' => 'required|max:30',
-            'prezzo' => 'required|min:3',
+            'prezzo' => 'required|min:3|numeric',
             'alimentazione' => 'required|max:20',
-            'anno' => 'required',
+            'anno' => 'required|numeric',
             'numero_telaio' => 'required|max:20|unique:cars',
             'trazione' => 'max:20',
+            'porte' => 'required|numeric',
             'carrozzeria' => 'max:20',
-            'cavalli' => 'required',
-            'cilindrata' => 'required',
+            'cavalli' => 'required|numeric',
+            'cilindrata' => 'required|numeric',
 
         ];
     }
@@ -44,16 +45,21 @@ class StoreCarRequest extends FormRequest
             'modello.max' => 'Il modello può essere composto da massimo 30 caratteri',
             'prezzo.required' => 'Il prezzo è obbligatorio',
             'prezzo.min' => 'Il prezzo minimo è di 100 euro',
+            'prezzo.numeric' => 'Il prezzo minimo è di 100 euro',
             'alimentazione.required' => 'L\'alimentazione è obbligatoria',
             'alimentazione.max' => 'L\'alimentazione può essere composta da un massimo di 20 caratteri ',
             'anno.required' => 'Inserire l\'anno',
+            'anno.numeric' => 'L\'anno deve essere un numero',
             'numero_telaio.required' => 'Inserire il numero di telaio',
             'numero_telaio.max' => 'Il numero di telaio devecontenere al massimo 20 caretteri',
             'numero_telaio.unique' => 'Il numero ditelaio deve essere unico, il seguente numero è già presente',
             'trazione.max' => 'Il tipo di trazione è composto da un massimo di 20  caratteri',
+            'porte.numeric' => 'Il numero di porte deve essere un numero',
             'carrozzeria.max' => 'Il tipo di carrozzeria consente un inserimento di massimo 20 caratteri',
             'cavalli.required' => 'Inserire il numerodi cavalli',
+            'cavalli.numeric' => 'cavalli deve essere un numero',
             'cilindrata.required' => 'Inserire la cilindrata',
+            'cilindrata.numeric' => 'cilindrata deve essere un numero',
         ];
     }
 }
