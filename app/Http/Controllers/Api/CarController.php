9c93c1f7ec513571f89d02c78b21e6115e19832c
stappost <emanuele.scarpantoni@gmail.com>
2024-03-07 10:18:30 +0100

    add slug in controllers

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\Car;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Str;
+
 class CarController extends Controller
 {
     public function index()
     {
         $cars = Car::all();
-
+        foreach($cars as $car){
+            $car['slug'] = Str::slug($car->modello, '-');
+        }
         return response()->json([
             "success" => true,
             "results" => $cars
